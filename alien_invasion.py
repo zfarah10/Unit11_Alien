9faf145139dbf81aaacb1460ed95ff4b22568dcd
zfarah10 <zfarah10@student.cscc.edu>
2025-07-13 04:13:04 -0400

    Created initial Game Loop and set up Pygame Display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,26 @@
 import sys
 import pygame
 
+class AlienInvasion:
+    def __init__(self) -> None:
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        
+        self.running = True
+
+    def run_game(self) -> None:
+        # Game Loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
