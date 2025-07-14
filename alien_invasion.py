db78a8a2d31e8cea78c7ad5cf62690b5c1e6d73d
zfarah10 <zfarah10@student.cscc.edu>
2025-07-14 02:06:43 -0400

    Created a settings file and changed the BG to ba an image scaled to the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,24 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self) -> None:
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_w,self.settings.screen_h)
+            )
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, 
+            (self.settings.screen_w, self.settings.screen_h)
+            )
         
         self.running = True
+        self.clock = pygame.time.Clock()
 
     def run_game(self) -> None:
         # Game Loop
@@ -18,8 +28,9 @@ class AlienInvasion:
                     self.running = False
                     pygame.quit()
                     sys.exit()
-
+            self.screen.blit(self.bg, (0,0))
             pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
 
 if __name__ == '__main__':
     ai = AlienInvasion()
