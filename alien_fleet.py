50c76f96c917f48bb0601727cbb6cfe32c5746c1
zfarah10 <zfarah10@student.cscc.edu>
2025-07-22 22:12:08 -0400

    p2- modified create fleet to also create all the rows

--- alien_fleet.py
+++ alien_fleet.py
@@ -17,28 +17,41 @@ class AlienFleet:
     
     def create_fleet(self) -> None:
         alien_w = self.settings.alien_w
+        alien_h = self.settings.alien_h
         screen_w = self.settings.screen_w
+        screen_h = self.settings.screen_h
 
-        fleet_w = self.calculate_fleet_size(alien_w, screen_w)
+        fleet_w, fleet_h = self.calculate_fleet_size(alien_w, screen_w, alien_h, screen_h)
 
-        # half_screen = self.settings.screen_w
+        half_screen = self.settings.screen_h//2
         fleet_horizontal_space = fleet_w * alien_w
+        fleet_vertical_space = fleet_h * alien_h
         x_offset = int((screen_w - fleet_horizontal_space)//2)
+        y_offset = int((half_screen-fleet_vertical_space)//2)
+        for row in range(fleet_h):
+            for col in range(fleet_w):
+                current_x = alien_w * col + x_offset
+                current_y = alien_h * row + y_offset
+                if col % 2 == 0 or row % 2 == 0:
+                    continue
+                self._create_alien(current_x, current_y)
 
-        for col in range(fleet_w):
-            current_x = alien_w * col + x_offset
-            if col % 2 == 0:
-                continue
-            self._create_alien(current_x, 10)
 
-
-    def calculate_fleet_size(self, alien_w, screen_w) -> any:
-        fleet_w = screen_w // alien_w
+    def calculate_fleet_size(self, alien_w, screen_w, alien_h, screen_h) -> any:
+        fleet_w = (screen_w // alien_w)
+        fleet_h = ((screen_h /2)//alien_h)
         if fleet_w % 2 == 0:
             fleet_w -= 1
         else:
             fleet_w -= 2
-        return fleet_w
+
+        if fleet_h % 2 == 0:
+            fleet_h -= 1
+        else:
+            fleet_h -= 2
+
+
+        return int(fleet_w), int(fleet_h)
     
 
     def _create_alien(self, current_x: int, current_y: int) -> None:
