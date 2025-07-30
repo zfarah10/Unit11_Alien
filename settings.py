9ac478b44657589a67d9e3ed4181ef1a3c87a1fd
zfarah10 <zfarah10@student.cscc.edu>
2025-07-30 19:12:19 -0400

    P3- Created Button for class and settings fonts

--- settings.py
+++ settings.py
@@ -42,3 +42,12 @@ class Settings:
         self.fleet_speed = 2
         self.fleet_direction = 1  # 1 = right, -1 = left
         self.fleet_drop_speed = 40
+
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0,135,50)
+
+        self.text_color = (255,255,255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf' 
