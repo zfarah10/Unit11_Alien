3b7c7340793aa61157d5ede6c197fa46adf55fc1
zfarah10 <zfarah10@student.cscc.edu>
2025-07-30 23:43:49 -0400

    p3 Finished up HUD to display lives and levels and plugged into Alien Invasion game

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             # update game stats level
             self.game_stats.update_level()
             # update HUD view
+            self.HUD.update_level()
 
     def _check_game_status(self):
         if self.game_stats.ships_left > 0:
--- hud.py
+++ hud.py
@@ -16,8 +16,16 @@ class HUD:
             self.settings.HUD_font_size)
         self.padding = 20
         self.update_scores()
-        # self.setup_life_image()
-        # self.update_level()
+        self._setup_life_image()
+        self.update_level()
+
+    def _setup_life_image(self) -> None:
+        self.life_image = pygame.image.load(self.settings.ship_file)
+        self.life_image = pygame.transform.scale(self.life_image, (
+            self.settings.ship_w, self.settings.ship_h
+            ))
+        self.life_rect = self.life_image.get_rect()
+
 
     def update_scores(self) -> None:
         self._update_max_score()
@@ -46,8 +54,25 @@ class HUD:
             self.settings.text_color, None)
         self.hi_score_rect = self.hi_score_image.get_rect()
         self.hi_score_rect.midtop = (self.boundaries.centerx,self.padding)
+    
+    def update_level(self) -> None:
+        level_str = f'Level: {self.game_stats.level: ,.0f}'
+        self.level_image = self.font.render(level_str, True,
+            self.settings.text_color, None)
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.boundaries.right - self.padding
+        self.level_rect.top = self.life_rect.bottom + self.padding
+
+    def _draw_lives(self) -> None:
+        current_x = self.padding
+        current_y = self.padding
+        for _ in range(self.game_stats.ships_left):
+            self.screen.blit(self.life_image, (current_x, current_y))
+            current_x += self.life_rect.width + self.padding
 
     def draw(self) -> None:
         self.screen.blit(self.hi_score_image,self.hi_score_rect)
         self.screen.blit(self.max_score_image,self.max_score_rect)
         self.screen.blit(self.score_image,self.score_rect)
+        self.screen.blit(self.level_image,self.level_rect)
+        self._draw_lives()
